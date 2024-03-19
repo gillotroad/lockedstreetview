a9b6ea6af58b5bd049dfed4be389fc25c13410ba
gillotroad <164035322+gillotroad@users.noreply.github.com>
2024-03-19 17:33:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,5 @@ function initPano() {
   );
 }
 
-        const searchParams = new URLSearchParams(window.location.search);
 
-        document.write("long = ");
-        document.write(searchParams.get('long'));
-
-        document.write("\n\n");
-        document.write("lat = ");
-        document.write(searchParams.get('lat'));
-
-
-//window.initPano = initPano;
+window.initPano = initPano;
