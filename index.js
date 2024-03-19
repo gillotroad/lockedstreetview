4ba64e2e3907e737334a8dc423d8bf5cd1d9423b
gillotroad <164035322+gillotroad@users.noreply.github.com>
2024-03-19 18:43:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
  * Copyright 2019 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/*
+Description:
+Opens Google Street View at desired location using latitude and longitude parameters.
+The Street View will have limited commands, specifically not showing links to the original Google Maps page.
+
+Manual:
+Call website passing arguments "lat" and "long" copied from Google Street View adress line.
+Example:
+https://gillotroad.github.io/lockedstreetview/?lat=38.721172&long=-106.7827289
+
+*/
+
 function initPano() {
   const searchParams = new URLSearchParams(window.location.search);
   const latparam = +searchParams.get('lat');
@@ -23,9 +36,10 @@ function initPano() {
   );
 
   
-  console.log("TESTNEU");
+  console.log("lat = ");
+  console.log(latparam);
   console.log("long = ");
-  console.log(searchParams.get('long'));
+  console.log(longparam);
 }
 
 
